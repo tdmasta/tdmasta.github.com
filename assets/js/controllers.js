418c779dffc8b9c41a0bdba483b53000e83e4485
Christophe Sarrouilhe <csarrouilhe@telecomdesign.fr>
2012-08-22 10:49:54 +0200

    http.get url test

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -8,10 +8,29 @@ function RegistrationCtrl($scope) {
 }
 
 function TopListCtrl($scope, $http) {
-    $http.get('/assets/js/controllers.js').
+    // $http.get('/assets/js/controllers.js').
     // $http.get('http://workflow.insgroup.fr/workflow/pv/gateways/overprod?currentstatus=OK&percent=100').
     // $http.get('http://briantford.com/blog/angular-d3.html').
     // $http.jsonp('http://api.ihackernews.com/page?format=jsonp&callback=JSON_CALLBACK').
+
+
+    $http({
+            method: 'GET',
+            url: 'http://192.168.0.42:9010/security/authentication',
+            params: {'email': $scope.account.email, 'password': $scope.account.email},
+            headers: {'Authorization': 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='}
+            }).
+        success(function(data, status) {
+            alert('++ OK : status = ' + status + ' et data = ' + data);
+        }).
+        error(function(data, status) {
+            alert('++ KO - request failed - status = ' + status + ' et data = ' + data);
+        });
+
+/*
+    $http.get('192.168.0.42:9010/security/authentication').
+    192.168.0.42:9010/security/authentication
+    /mcs/devices
         success(function(data, status) {
             $scope.posts = data;
             alert('>>> OK : status = ' + status + ' et data = ' + data);
@@ -19,4 +38,5 @@ function TopListCtrl($scope, $http) {
         error(function(data, status) {
             alert('>>> KO - request failed - status = ' + status + ' et data = ' + data);
         });
+*/
 }
