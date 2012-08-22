c73b71573108b30564a25781999ae28765ea71b3
Christophe Sarrouilhe <csarrouilhe@telecomdesign.fr>
2012-08-22 10:07:59 +0200

    http.get test

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -8,24 +8,15 @@ function RegistrationCtrl($scope) {
 }
 
 function TopListCtrl($scope, $http) {
-/**
-    $http.jsonp('http://api.ihackernews.com/page?format=jsonp&callback=JSON_CALLBACK').
-        success(function(data) {
+    $http.get('/assets/js/controllers.js').
+    // $http.get('http://workflow.insgroup.fr/workflow/pv/gateways/overprod?currentstatus=OK&percent=100').
+    // $http.get('http://briantford.com/blog/angular-d3.html').
+    // $http.jsonp('http://api.ihackernews.com/page?format=jsonp&callback=JSON_CALLBACK').
+        success(function(data, status) {
             $scope.posts = data;
+            alert('>>> OK : status = ' + status + ' et data = ' + data);
         }).
-        error(function(data) {
-            alert('Error during http.jsonp callback');
-        })
-*/
-$http.get('/assets/js/controllers.js').
-// $http.get('http://workflow.insgroup.fr/workflow/pv/gateways/overprod?currentstatus=OK&percent=100').
-// $http.get('http://briantford.com/blog/angular-d3.html').
-// $http.jsonp('http://api.ihackernews.com/page?format=jsonp&callback=JSON_CALLBACK').
-    success(function(data, status) {
-        $scope.posts = data;
-        alert('>>> OK : status = ' + status + ' et data = ' + data);
-    }).
-    error(function(data, status) {
-        alert('>>> KO - request failed - status = ' + status + ' et data = ' + data);
-    });
+        error(function(data, status) {
+            alert('>>> KO - request failed - status = ' + status + ' et data = ' + data);
+        });
 }
