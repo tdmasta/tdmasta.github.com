23d612feb03ba017d98faa2af8921aa256e7bbbd
Christophe Sarrouilhe <csarrouilhe@telecomdesign.fr>
2012-08-24 09:21:13 +0200

    dashboard init page

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -70,9 +70,9 @@ function RegistrationCtrl($scope, $http) {
 }
 
 // Dashboard controller
-function DashboardCtrl($scope, $http) {
+function DashboardCtrl($scope) {
     $scope.items = [];
-    
+
     var counter = 0;
     $scope.loadMore = function() {
         for (var i = 0; i < 5; i++) {
@@ -80,13 +80,13 @@ function DashboardCtrl($scope, $http) {
             counter += 10;
         }
     };
-    
     $scope.loadMore();
 }
+
 angular.module('scroll', []).directive('whenScrolled', function() {
     return function(scope, elm, attr) {
         var raw = elm[0];
-        
+
         elm.bind('scroll', function() {
             if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
                 scope.$apply(attr.whenScrolled);
