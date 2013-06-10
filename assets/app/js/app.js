2b5e7dc3921f76e88c37f0de88dd7204380527d4
Frederic Nobilleau <fnobilleau@telecomdesign.fr>
2013-06-10 11:19:36 +0200

    correction url

--- assets/app/js/app.js
+++ assets/app/js/app.js
@@ -81,5 +81,5 @@ app.run(function($log, $cookieStore, Context, $timeout) {
 });
 
 //app.constant('CONSTANTS', {remote : 'http://localhost:9010'});
-//app.constant('CONSTANTS', {remote : 'https://sensor.insgroup.fr'});
-app.constant('CONSTANTS', {remote : 'http://192.168.1.14:9010'});
+app.constant('CONSTANTS', {remote : 'https://sensor.insgroup.fr'});
+//app.constant('CONSTANTS', {remote : 'http://192.168.1.14:9010'});
